736df91648b8f61f6f48cf5daa1881efe9cca7c2
cudmore <robert.cudmore@gmail.com>
2019-05-20 07:43:58 -0700

    dash browser

---
+++ dash/bBrowser.py
@@ -0,0 +1,270 @@
+'''
+Author: RObert Cudmore
+Date: 20190518
+
+Purpose:
+   Create a web based data browser of all bAnalysis files in a folder
+'''
+
+import os, collections
+
+import pandas as pd
+
+import dash
+import dash_table
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
+import plotly.graph_objs as go
+
+class bBrowser:
+	def __init__(self, path=''):
+		if path == '':
+			path = '/Users/cudmore/Sites/bAnalysis/data'
+		self.path = path
+		
+		self.df0 = None # small dataframe with list of files loaded
+		
+		self.df = None # massive dataframe with all spikes across all file
+		
+	def loadFolder(self):
+		"""
+		Load a hard drive folder of bAnalysis output files (e.g. .txt files) into one Pandas dataframe (df)
+		"""
+		if not os.path.isdir(self.path):
+			print('error: bBrowser.loadFolder() did not find path:', self.path)
+			return
+			
+		columns = ['Index',
+			'Analysis File',
+			'ABF File',
+			'Num Spikes',
+			'First Spike (Sec)',
+			'Last Spike (sec)'
+		]
+		self.df0 = pd.DataFrame(columns=columns)
+		
+		df0_list = []
+		
+		currIdx = 0
+		for file in os.listdir(self.path):
+			if file.startswith('.'):
+				continue
+			if file.endswith('.txt'):
+				currFile = os.path.join(self.path,file)
+				print('bBrowser.loadFolder() loading analysis file:', currFile)
+				df = pd.read_csv(currFile, header=0) # load comma seperated values, read header names from row 1
+				df.insert(0, 'Analysis File', file)
+
+				numSpikes = len(df.index)
+				firstSpikeSec = df['thresholdSec'].min()
+				lastSpikeSec = df['thresholdSec'].max()
+				abfFile = df.iloc[0]['file']
+				#abfFile = os.path.split(abfFile)[1] # get the file name from the path
+				
+				dfRow = collections.OrderedDict()
+				dfRow['Index'] = currIdx + 1
+				dfRow['Analysis File'] = file
+				dfRow['ABF File'] = abfFile
+				dfRow['Num Spikes'] = numSpikes
+				dfRow['First Spike (Sec)'] = firstSpikeSec
+				dfRow['Last Spike (Sec)'] = lastSpikeSec
+
+				df0_list.append(dfRow)
+				
+				# BIG df with ALL spikes
+				if self.df is None:
+					self.df = df
+				else:
+					self.df = pd.concat([self.df, df], axis=0)
+								
+				currIdx += 1
+			
+		self.df0 = pd.DataFrame(df0_list)
+	
+	def getStatList(self):
+		skipColumns = [''] #['file', 'spikeNumber', 'numError', 'errors', 'dVthreshold', 'medianFilter', 'halfHeights']
+		retList = []
+		mySpikeNumber = 0
+		for column in self.df:
+			if column in skipColumns:
+				continue
+			optionsDict = {
+				'label': column,
+				'value': column
+			}
+			retList.append(optionsDict)
+		return retList
+		
+	def updatePlot(self, xStatName, yStatName):
+		""" return a list of dict for plotly/dash data"""
+		
+		theRet = []
+		
+		if xStatName and yStatName:
+			for index, row in self.df0.iterrows():
+				#print(index, row['ABF File'])
+				#print(index, row)
+		
+				analysisFile = row['Analysis File']
+				abfFile = row['ABF File']
+
+				thisFileRows = self.df.loc[self.df['Analysis File'] == analysisFile]
+
+				# get rows in self.df corresponding to abfFile
+				xStatVals = thisFileRows[xStatName]
+				yStatVals = thisFileRows[yStatName]
+			
+				dataDict = {}
+				dataDict['x'] = xStatVals
+				dataDict['y'] = yStatVals
+				dataDict['mode'] = 'markers'
+				dataDict['name'] = analysisFile + ':' + abfFile
+			
+				theRet.append(dataDict)
+			
+		return theRet
+		
+myBrowser = bBrowser()
+myBrowser.loadFolder()
+
+#print('myBrowser.getStatList():', myBrowser.getStatList())
+myStatList = myBrowser.getStatList()
+
+newData = myBrowser.updatePlot(xStatName='thresholdSec', yStatName='thresholdVal')
+
+app = dash.Dash(__name__)
+
+"""
+			'''
+			'data': [
+				{
+					'x': [1, 2, 3, 4],
+					'y': [4, 1, 3, 5],
+					'text': ['a', 'b', 'c', 'd'],
+					'customdata': ['c.a', 'c.b', 'c.c', 'c.d'],
+					'name': 'Trace 1',
+					'mode': 'markers',
+					'marker': {'size': 12}
+				},
+				{
+					'x': [1, 2, 3, 4],
+					'y': [9, 4, 1, 4],
+					'text': ['w', 'x', 'y', 'z'],
+					'customdata': ['c.w', 'c.x', 'c.y', 'c.z'],
+					'name': 'Trace 2',
+					'mode': 'markers',
+					'marker': {'size': 12}
+				}
+			],
+			'''
+"""
+
+app.layout = html.Div([
+	dash_table.DataTable(
+		id='analysis-table',
+		row_selectable='multi',
+		columns=[{"name": i, "id": i} for i in myBrowser.df0],
+		#data=df.to_dict('records'),
+		data=myBrowser.df0.to_dict('records'),
+	),
+
+
+	html.Div([
+		html.Div([
+		dash_table.DataTable(
+			id='y-datatable',
+			columns=[{"name": 'Idx', "id": 'Idx'}, {"name": 'Stat', "id": 'stat'}],
+			data=[{"Idx": idx+1, "stat": myOption['label']} for idx, myOption in enumerate(myStatList)],
+			style_table={
+				'maxWidth': '300',
+				'maxHeight': '300',
+				'overflowY': 'scroll'
+			},
+			style_cell={'textAlign': 'left'},
+			style_header={
+				'backgroundColor': 'ltgray',
+				'fontWeight': 'bold'
+			},
+		),
+		], style={'width': '50%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+
+		html.Div([
+		dash_table.DataTable(
+			id='x-datatable',
+			columns=[{"name": 'Idx', "id": 'Idx'}, {"name": 'Stat', "id": 'stat'}],
+			data=[{"Idx": idx+1, "stat": myOption['label']} for idx, myOption in enumerate(myStatList)],
+			style_table={
+				'maxWidth': '300',
+				'maxHeight': '300',
+				'overflowY': 'scroll'
+			},
+			style_cell={'textAlign': 'left'},
+			style_header={
+				'backgroundColor': 'ltgray',
+				'fontWeight': 'bold'
+			},
+		),
+		], style={'width': '50%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+	], className='two columns'),
+
+		
+	dcc.Graph(
+		id='life-exp-vs-gdp',
+		figure={
+			'data': newData, # data is a list of go.Scatter
+			'layout': {
+				#'title': 'xxxyyy',
+				'xaxis': {
+					'title':'SP'
+				},
+				'yaxis': {
+					'title':'Number of issues'
+				},
+				'margin':{'l': 50, 'b': 50, 't': 50, 'r': 50},
+				'clickmode':'event+select'
+			}
+		}
+	),
+
+	]) # closing app.layout = html.Div([
+
+#
+# callbacks
+#
+@app.callback(
+	Output('life-exp-vs-gdp', 'figure'),
+	[
+	Input('y-datatable', 'active_cell'),
+	Input('x-datatable', 'active_cell'),
+	])
+def myTableSelect(y_activeCell, x_activeCell):
+	xSel, ySel = None, None
+	if y_activeCell is not None:
+		yRow = y_activeCell[0]
+		ySel = myStatList[yRow]['label']
+	if x_activeCell is not None:
+		xRow = x_activeCell[0]
+		xSel = myStatList[xRow]['label']
+	print('myTableSelect ySel:', ySel, 'xSrl:', xSel)
+	
+	returnData = myBrowser.updatePlot(xStatName=xSel, yStatName=ySel)
+
+	return {
+		'data': returnData, # data is a list of go.Scatter
+		'layout': {
+			#'title': 'xxxyyy',
+			'xaxis': {
+				'title':xSel
+			},
+			'yaxis': {
+				'title':ySel
+			},
+			'margin':{'l': 50, 'b': 50, 't': 50, 'r': 50},
+			'clickmode':'event+select'
+		}
+	}
+
+if __name__ == '__main__':
+
+	app.run_server(debug=True)
---
+++ dash/readme.md
@@ -0,0 +1,3 @@
+## Plotly Dash experiments
+
+These python files provide a web interface to the data.
