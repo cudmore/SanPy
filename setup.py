19e5d7238703bc3c173e900d461a542f2c11c1ab
cudmore <robert.cudmore@gmail.com>
2020-07-18 14:25:31 -0700

    fixing install and adding setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup#, find_packages
+
+#exec (open('bimpy/version.py').read())
+
+setup(
+    name='sanpy',
+    version='0.1',
+    description='Cardiac Myocyte Current Clamp Analysis',
+    url='http://github.com/cudmore/SanPy',
+    author='Robert H Cudmore',
+    author_email='robert.cudmore@gmail.com',
+    license='GNU GPLv3',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas',
+        'PyQt5',
+        'pyqtgraph',
+        'matplotlib',
+        'pyabf',
+        'XlsxWriter',
+    ]
+)
