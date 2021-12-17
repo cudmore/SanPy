e5dad76da3a561d8549f49c70c558e09a7046515
cudmore <robertcudmore@gmail.com>
2021-12-17 14:34:34 -0800

    heroku

--- sanpy/__init__.py
+++ sanpy/__init__.py
@@ -3,37 +3,23 @@
 
 # only export base sanpy, no GUI
 from .bAnalysis_ import *
-#from .bAnalysis import bAnalysis
-#from .bAnalysis import getHalfWidths
-
 from .analysisDir import *
-#from .analysisDir import sanpyColumns
-
-#from .analysisPlot import bAnalysisPlot
-#from .analysisPlot import getEddLines
-#from .bAnalysisPlot import getHalfWidths
 from .analysisPlot import *
-
-# 20211025
-#from .bAnalysisUtil import bAnalysisUtil
-#from .bAnalysisUtil import statList
 from .bAnalysisUtil import *
+from .atfStim import *
+from .bAnalysisResults import *
 
 from .bAbfText import bAbfText
-
 from .bExport import bExport
 
 from .version import analysisVersion
 from .version import interfaceVersion
 
-from .atfStim import *
-
 from .bDetection import bDetection
-from .bAnalysisResults import *
 
 # hold off on loading from aws for now (requires boto3)
 #from .awsUtil import *
 
 # need this for heroku ????????????????????????????????????????????????????
-#from .userAnalysis import *
+from .userAnalysis import *
 #from .userAnalysis import userAnalysis
