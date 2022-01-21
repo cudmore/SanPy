535bd81b82926d9814a9be3970877e321cacc547
cudmore <robert.cudmore@gmail.com>
2022-01-21 09:56:42 -0800

    rabbit kymograph

---
+++ sanpy/interface/bKymograph.py
@@ -0,0 +1,15 @@
+"""
+Display a tif kymograph and allow user to modify a rect roi
+"""
+
+import numpy as np
+
+from PyQt5 import QtCore, QtWidgets, QtGui
+import pyqtgraph as pg
+
+class kWidget(QtWidgets.QWidget):
+	def __init__(self, tifPath):
+		pass
+		
+if __name__ == '__main__':
+	pass
