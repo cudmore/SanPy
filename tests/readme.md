7b2600010384bed1c13c6c61a6c6ef9ab7e5a1fa
Robert Cudmore <cudmore@Roberts-MacBook-Pro-3.local>
2022-12-24 15:47:10 -0800

    refactored user analysis to proper class (no static)

---
+++ tests/readme.md
@@ -0,0 +1,11 @@
+Run these locally with
+
+```
+python -m unittest
+```
+
+or use `-s` to print logging to terminal
+
+```
+pytest -s sanpy/tests
+```
---
+++ tests/test_analysis.py
@@ -0,0 +1,54 @@
+"""
+Test bAnalysis.py
+
+Run this file with
+
+```
+python -m unittest sanpy/tests/test_analysis.py
+```
+"""
+
+import os, shutil, tempfile
+import unittest
+
+import sanpy
+
+import logging
+from sanpy.sanpyLogger import get_logger
+logger = get_logger(__name__, level=logging.DEBUG)
+
+class Test_Analysis(unittest.TestCase):
+    # this patter is wierd to me?
+    path = 'data/19114001.abf'
+    ba = sanpy.bAnalysis(path)
+    expectedNumSpikes = 103
+    expectedNumErrors = 69
+
+    def test_0_load(self):
+        logger.info('RUNNING')
+
+        self.assertFalse(self.ba.loadError)
+        self.assertIsNotNone(self.ba.sweepX)
+        self.assertIsNotNone(self.ba.sweepY)
+        self.assertEqual(len(self.ba.sweepX), len(self.ba.sweepY))
+
+    def test_1_detect(self):
+        logger.info('RUNNING')
+        # grab detection parameters
+        # see: sanpy.bDetection.detectionPresets.cakymograph
+        bd = sanpy.bDetection()  # gets default
+        dDict = bd.getDetectionDict('SA Node')
+        # detect
+        self.ba.spikeDetect(dDict)
+
+        self.assertEqual(self.ba.numSpikes, self.expectedNumSpikes) # expecting 102 spikes
+        self.assertEqual(len(self.ba.dfError), self.expectedNumErrors) # expecting 102 spikes
+
+    def test_2_stats(self):
+        logger.info('RUNNING')
+        thresholdSec = self.ba.getStat('thresholdSec')
+
+        self.assertEqual(len(thresholdSec), self.expectedNumSpikes) # expecting 102 spikes
+
+if __name__ == '__main__':
+    unittest.main()
