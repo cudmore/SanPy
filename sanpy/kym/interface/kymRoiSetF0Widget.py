8ca7296e5e6d9da4b01f0be3d4302fb11f87f9c5
Robert Cudmore <robert.cudmore@gmail.com>
2025-05-27 19:07:08 -0700

    lots of work on colin kym

---
+++ sanpy/kym/interface/kymRoiSetF0Widget.py
@@ -0,0 +1,231 @@
+from typing import Optional
+
+import numpy as np
+
+from PyQt5 import QtGui, QtCore, QtWidgets
+import pyqtgraph as pg
+
+from sanpy.interface.util import sanpyCursors
+
+from sanpy.kym.kymRoiDetection import KymRoiDetection
+from sanpy.kym.kymRoiAnalysis import KymRoiAnalysis, PeakDetectionTypes
+
+from sanpy.sanpyLogger import get_logger
+logger = get_logger(__name__)
+
+class SetF0Widget(QtWidgets.QWidget):
+    """Class to display sum intensity to show and manually set f0.
+    """
+
+    signalUpdateF0 = QtCore.pyqtSignal(object, object, object)  # (channel, roiLabel, new_f0_value)
+
+    def __init__(self, kymRoiAnalysis : KymRoiAnalysis):
+        super().__init__()
+
+        self._kymRoiAnalysis = kymRoiAnalysis
+        
+        self.xTrace = 'Time (s)'
+        self.yTrace = 'intDetrend'
+
+        self._kymRoiDetection : KymRoiDetection = None
+        """Switches to one channel in slot_selectRoi().
+        """
+        
+        self._channelIdx : Optional[int] = None
+        self._roiLabel : Optional[str] = None
+
+        self._buildUI()
+
+    def slot_selectRoi(self, channelIdx : int, roiLabel : Optional[str]):
+        logger.info(f'channelIdx:{channelIdx} roiLabel:{roiLabel}')
+
+        if roiLabel is None:
+            # clear the plot
+            # yEmpty = [np.nan] * self._kymRoiAnalysis.numLineScans
+            logger.info('clearing f0 plot')
+            self.intensityPlotItem.setData(np.array([]), np.array([]))
+
+            self._channelIdx = None
+            self._roiLabel = None
+            self._kymRoiDetection = None
+
+            return
+        
+        self._channelIdx = channelIdx
+        self._roiLabel = roiLabel
+        
+        # backend KymRoi
+        kymRoi = self._kymRoiAnalysis.getRoi(roiLabel)
+        self._kymRoiDetection = kymRoi.getDetectionParams(channelIdx, PeakDetectionTypes.intensity)
+
+        xPlot = kymRoi.getTrace(channelIdx, self.xTrace)
+        yPlot = kymRoi.getTrace(channelIdx, self.yTrace)
+
+        self.intensityPlotItem.setData(xPlot, yPlot)
+        
+        # if channelIdx == 0:
+        #     _color = '#DD1111'
+        # else:
+        #     _color = 'Green'
+        _color = self._kymRoiAnalysis.getChannelColor(channelIdx)
+
+        self.intensityPlotItem.setPen(pg.mkPen(color=_color))
+
+        self.intensityPlotWidget.setLabel("left", 'Sum Intensity', color=_color, units="")
+
+        # set horizontal line for f0 percentile (pg.InfiniteLine)
+        f0_percentile = self._kymRoiDetection['f0 Value Percentile']
+        if f0_percentile is not None:
+            # f0Value = kymRoi.getTrace(channelIdx, 'f0 Value Percentile')
+            self.rawIntensity_f0_line.setPos(f0_percentile)
+            # logger.error(f'infinite line has no set label, want to set to f0_percentile:{f0_percentile:.2f}')
+            # self.rawIntensity_f0_line.setLabel(f'f0={f0_percentile:.2f}')
+            self.rawIntensity_f0_line.setVisible(True)
+        else:
+            self.rawIntensity_f0_line.setVisible(False)
+
+        # set cursor c to manual f0
+        f0_manual = self._kymRoiDetection['f0 Value Manual']
+        self._sanpyCursors._cursorC.setPos(f0_manual)
+
+    def myContextMenu(self, event):
+        """Context menu for raw plot item.
+        
+        Used to set f0 from cursors.
+        See also _contextMenu() for global widget context menus.
+        """
+        if self._kymRoiDetection is None:
+            return
+        
+        contextMenu = QtWidgets.QMenu()
+        
+        contextMenu.addAction('Full Zoom')
+        contextMenu.addSeparator()
+
+        _cursorsShowing = self._sanpyCursors.cursorsAreShowing()
+        
+        cCursorValue = self._sanpyCursors._cCursorVal
+        cCursorValue = round(cCursorValue, 2)  # round
+
+        cursorAction = QtWidgets.QAction('Cursors')
+        cursorAction.setCheckable(True)
+        cursorAction.setChecked(_cursorsShowing)
+        contextMenu.addAction(cursorAction)
+        contextMenu.addSeparator()
+
+        #
+        f0ManualPercentile = self._kymRoiDetection['f0 Type']  # in (Manual, Percentile)
+        logger.info(f'f0ManualPercentile:"{f0ManualPercentile}"')
+        _do_f0_Manual = f0ManualPercentile == 'Manual'
+        
+        f0Action = QtWidgets.QAction(f'Set f0 to {cCursorValue}')
+        f0Action.setEnabled(_cursorsShowing and _do_f0_Manual)
+        contextMenu.addAction(f0Action)
+
+        action = contextMenu.exec_(event.globalPos())
+        if action is None:
+            return
+        
+        # respond to menu selection
+        _ret = ''
+        actionText = action.text()
+        if actionText == 'Full Zoom':
+            self._resetZoom()
+            _ret = 'Reset zoom'
+
+        elif actionText == 'Cursors':
+            _checked = cursorAction.isChecked()
+            # self._sanpyCursors.toggleCursors(_checked)
+            self._sanpyCursors.toggleCursors(_checked)
+
+        elif action == f0Action:
+            _ret = f'User set f0 to {cCursorValue}'
+            logger.info(_ret)
+            
+            self._kymRoiDetection['f0 Value Manual'] = cCursorValue
+            logger.info(f'   --->>> emit _channelIdx:{self._channelIdx} _roiLabel:{self._roiLabel} cCursorValue:{cCursorValue}')
+            self.signalUpdateF0.emit(self._channelIdx, self._roiLabel, cCursorValue)
+
+        self.mySetStatusbar(_ret)
+
+    def _resetZoom(self):
+        self.intensityPlotItem.autoRange()
+
+    def _setXLink(self, widget):
+        self.intensityPlotWidget.setXLink(widget)
+
+    def _buildUI(self):
+        vBoxPlot = QtWidgets.QVBoxLayout()
+        vBoxPlot.setAlignment(QtCore.Qt.AlignTop)
+        self.setLayout(vBoxPlot)
+
+        self.intensityPlotWidget = pg.PlotWidget()
+        vBoxPlot.addWidget(self.intensityPlotWidget)
+
+        self.intensityPlotWidget.setDefaultPadding()
+        self.intensityPlotWidget.enableAutoRange()
+        self.intensityPlotWidget.setMouseEnabled(x=True, y=False)
+        self.intensityPlotWidget.hideButtons()  # hide the little 'A' button to rescale axis
+
+        # abb 20250501
+        logger.warning('hard coding getChannelColor to 0')
+        _channelColor = self._kymRoiAnalysis.getChannelColor(0)
+
+        self.intensityPlotWidget.setLabel("left", 'Sum Intensity', color=_channelColor, units="")
+        self.intensityPlotWidget.setLabel("bottom", 'Time (s)', units="")
+
+        # get the original font and make it bigger
+        _origFont = self.intensityPlotWidget.getAxis("bottom").label.font()
+        from sanpy.kym.interface.kymRoiWidget import KymRoiWidget  # just for font size
+        _origFont.setPointSize(KymRoiWidget._pgAxisLabelFontSize)
+        self.intensityPlotWidget.getAxis("bottom").label.setFont(_origFont)
+        self.intensityPlotWidget.getAxis("left").label.setFont(_origFont)
+
+        # self.intensityPlotWidget.setXLink(self._kymRoiImageWidget.kymographPlot)
+
+        # re-wire right-click (for entire widget)
+        # self.intensityPlotWidget.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        # self.intensityPlotWidget.customContextMenuRequested.connect(self.myContextMenu)
+        self.intensityPlotWidget.contextMenuEvent = self.myContextMenu  # rewire right-click to custom function
+
+        self.intensityPlotItem = pg.PlotCurveItem(pen=pg.mkPen(_channelColor, width=2))
+        self.intensityPlotItem.setData(x=self._kymRoiAnalysis.getXAxis(), y=[np.nan]*self._kymRoiAnalysis.numLineScans)
+        self.intensityPlotWidget.addItem(self.intensityPlotItem)
+
+        # horizontal line to show f0
+        self.rawIntensity_f0_line = pg.InfiniteLine(angle=0,
+                                                    movable=False,
+                                                    pen = pg.mkPen('c', width=2),
+                                                    # label=f'f0={f0Value}',
+                                                    label='f0 %:{value:.2f}',  # hidden variable value is value of line
+                                                    # labelText='f0={value:.2f}',
+                                                    # labelOpts={'position':0.05},
+                                                    # labelOpts={'position':0.85, 'color':'c', 'font-size':'10pt'},
+                                                    labelOpts={'position':0.85, 'color':'c'},
+                                                    )
+        self.intensityPlotWidget.addItem(self.rawIntensity_f0_line)
+
+        # a vertical line to show selected line scan
+        self._kymLineScanLine = pg.InfiniteLine(pen=pg.mkPen(color='c', width=2))
+        self.intensityPlotWidget.addItem(self._kymLineScanLine)
+
+        # cursors for user to set f0 manual
+        # self._sanpyCursors._cursorC is a pg iinfinite line
+        self._sanpyCursors = sanpyCursors(self.intensityPlotWidget,
+                                          showCursorD=False,
+                                          cursorC_label='f0 Manual:',)
+        self._sanpyCursors._showCursorA = False
+        self._sanpyCursors._showCursorB = False
+        self._sanpyCursors.toggleCursors(True)  # initially visible
+        self._sanpyCursors.signalCursorDragged.connect(self.mySetStatusbar)
+
+    def slot_updateLineProfile(self, lineScanIdx : int):
+        """Update vertical line showing current selected line scan.
+        """
+        # logger.info(f'lineScanIdx:{lineScanIdx}')
+        lineScanSec = lineScanIdx * self._kymRoiAnalysis.secondsPerLine
+        self._kymLineScanLine.setPos(lineScanSec)
+
+    def mySetStatusbar(self, text : str):
+        # logger.info('')
+        pass
---
+++ sanpy/kym/interface/kymRoiToolbar.py
@@ -0,0 +1,189 @@
+
+from typing import Optional
+from functools import partial
+
+from PyQt5 import QtCore, QtWidgets
+
+import qtawesome as qta
+
+from sanpy.kym.kymRoiAnalysis import KymRoiAnalysis
+from sanpy.kym.kymRoiDetection import KymRoiDetection
+
+from sanpy.sanpyLogger import get_logger
+logger = get_logger(__name__)
+
+class KymRoiGroupBox(QtWidgets.QGroupBox):
+    """
+    A widget to display and edit an roi.
+    """
+    signalRoiChanged = QtCore.pyqtSignal(object)  # (roi label)
+
+    def __init__(self,
+                 kymRoiAnalysis : KymRoiAnalysis,
+                 kymRoiDetection : KymRoiDetection,
+                 groupName : str,
+    ):
+
+        super().__init__(title=groupName)
+
+        self._kymRoiAnalysis = kymRoiAnalysis
+        self._kymRoiDetection = kymRoiDetection
+        self._groupName = groupName
+
+        self._roiLabel = None  # set in slot_selectRoi()
+
+        self._blockSlots = False
+
+        self._buildUI()
+
+    def slot_rio_changed(self, roiLabel : str):
+        logger.warning('hard coding channel=0 ???')
+        self.slot_selectRoi(channel=0, roiLabel=roiLabel)
+
+    def slot_selectRoi(self, channel : int, roiLabel : Optional[str]):
+        logger.info(f'channel:{channel} roiLabel:{roiLabel}')
+
+        # always setTitle()
+        _title = f'{self._groupName} ch {channel+1} roi {roiLabel}'
+        self.setTitle(_title)
+
+        self._roiLabel = roiLabel
+
+        if roiLabel is not None:
+            self.setEnabled(True)
+            
+            roi = self._kymRoiAnalysis.getRoi(roiLabel)
+            _rectDict = roi.getRectDict()
+            logger.info(f'  roi _rectDict:{_rectDict} ')  # pixels like [0, 512, 2000, 0]
+
+            self._widgetDict['leftLabel'].setText(f"Left: {_rectDict['left']}")
+            self._widgetDict['topLabel'].setText(f"Top: {_rectDict['top']}")
+            self._widgetDict['rightLabel'].setText(f"Right: {_rectDict['right']}")
+            self._widgetDict['bottomLabel'].setText(f"Bottom: {_rectDict['bottom']}")
+
+            # set value of spinbox
+            self._blockSlots = True  # TODO: use context manager
+            _height = _rectDict['top'] - _rectDict['bottom']
+            self._widgetDict['setHeight'].setValue(_height)
+            self._blockSlots = False
+
+        else:
+            self.setEnabled(False)
+
+    def _on_spin_box(self, name, value):
+        """
+        Slot to handle spin box value changes.
+        """
+        if self._blockSlots:
+            logger.warning('blocked')
+    
+        logger.info(f'spin box "{name}" value:{value}')
+
+        if name == 'Height':
+            # set backend height of roi in pixels
+            self._kymRoiAnalysis.getRoi(roiLabel=self._roiLabel).setRoiHeightPixels(value)
+
+        logger.info(f'-->> emit signalRoiChanged roi label:{self._roiLabel}')
+        self.signalRoiChanged.emit(self._roiLabel)
+
+    def _buildUI(self):
+        """
+        Build the UI for the group box.
+        """
+        self.setEnabled(False)
+        
+        self._widgetDict = {}
+        
+        # self.setTitle(self._groupName)
+        # self.setCheckable(True)
+        # self.setChecked(True)
+    
+        # self.setContentsMargins(self._contentMarginLeft, 0, 0, 0)
+
+        # Create a layout for the group box
+        mainLayout = QtWidgets.QVBoxLayout()
+        mainLayout.setAlignment(QtCore.Qt.AlignTop)
+        self.setLayout(mainLayout)  # main layout for self groupbox (inherited)
+
+
+        _hBox1 = QtWidgets.QHBoxLayout()
+        _hBox1.setAlignment(QtCore.Qt.AlignLeft)
+        mainLayout.addLayout(_hBox1)
+
+        _leftLabel = QtWidgets.QLabel("Left")
+        _topLabel = QtWidgets.QLabel("Top")
+        _rightLabel = QtWidgets.QLabel("Right")
+        _bottomLabel = QtWidgets.QLabel("Bottom")
+
+        _hBox1.addWidget(_leftLabel)
+        _hBox1.addWidget(_topLabel)
+        _hBox1.addWidget(_rightLabel)
+        _hBox1.addWidget(_bottomLabel)
+
+        # leftLabel.setAlignment(QtCore.Qt.AlignLeft)
+        # leftLabel.setContentsMargins(self._contentMarginLeft, 0, 0, 0)
+        self._widgetDict['leftLabel'] = _leftLabel
+        self._widgetDict['topLabel'] = _topLabel
+        self._widgetDict['rightLabel'] = _rightLabel
+        self._widgetDict['bottomLabel'] = _bottomLabel
+
+        # set height of roi in pixels
+        _hBox2 = QtWidgets.QHBoxLayout()
+        _hBox2.setAlignment(QtCore.Qt.AlignLeft)
+        mainLayout.addLayout(_hBox2)
+
+        spinBoxName= 'Height'
+        _heightSpinBox = QtWidgets.QSpinBox()
+        _heightSpinBox.setRange(0, 10000)
+        _heightSpinBox.setValue(0)
+        _heightSpinBox.setSingleStep(1)
+        _heightSpinBox.setPrefix(f'{spinBoxName} ')
+        _heightSpinBox.setSuffix(" (pixels)")
+        _heightSpinBox.setAlignment(QtCore.Qt.AlignLeft)
+        #
+        _heightSpinBox.setKeyboardTracking(False)
+        _heightSpinBox.valueChanged.connect(
+            partial(self._on_spin_box, spinBoxName)
+            )
+        #
+        self._widgetDict['setHeight'] = _heightSpinBox
+        _hBox2.addWidget(_heightSpinBox)
+
+        # buttons for nudge roi left/right/up/down
+        _nudgeLeftButton = QtWidgets.QPushButton(qta.icon('mdi6.arrow-left'), '')
+        _nudgeLeftButton.setToolTip("Nudge Left")
+        _nudgeRightButton = QtWidgets.QPushButton(qta.icon('mdi6.arrow-right'), '')
+        _nudgeRightButton.setToolTip("Nudge Right")
+        _nudgeUpButton = QtWidgets.QPushButton(qta.icon('mdi6.arrow-up'), '')
+        _nudgeUpButton.setToolTip("Nudge Up")
+        _nudgeDownButton = QtWidgets.QPushButton(qta.icon('mdi6.arrow-down'), '')
+        _nudgeDownButton.setToolTip("Nudge Down")
+        _hBox2.addWidget(_nudgeLeftButton)
+        _hBox2.addWidget(_nudgeRightButton)
+        _hBox2.addWidget(_nudgeUpButton)
+        _hBox2.addWidget(_nudgeDownButton)
+        _hBox2.addStretch(1)
+        # connect buttons to slots
+        _nudgeLeftButton.clicked.connect(
+            partial(self.nudgeRoi, direction='left')
+            )
+        _nudgeRightButton.clicked.connect(
+            partial(self.nudgeRoi, direction='right')
+            )
+        _nudgeUpButton.clicked.connect(
+            partial(self.nudgeRoi, direction='up')
+            )
+        _nudgeDownButton.clicked.connect(
+            partial(self.nudgeRoi, direction='down')
+            )
+
+    def nudgeRoi(self, direction : str):
+        """
+        Nudge the roi in the specified direction.
+        """
+        logger.info(f'nudgeRoi {self._roiLabel} {direction}')
+        # self._kymRoiDetection.nudgeRoi(roiLabel=roiLabel, direction=direction)
+        self._kymRoiAnalysis.getRoi(roiLabel=self._roiLabel).nudge(direction)
+
+        logger.info(f'-->> emit signalRoiChanged roi label:{self._roiLabel}')
+        self.signalRoiChanged.emit(self._roiLabel)
