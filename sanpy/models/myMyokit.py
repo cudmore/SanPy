566a70d35b91b41391c94cd64310382a6cd0f652
Robert Cudmore <robert.cudmore@gmail.com>
2026-09-10 18:12:59 -0400

    updated myMyokit.py

--- sanpy/models/myMyokit.py
+++ sanpy/models/myMyokit.py
@@ -10,66 +10,71 @@ import pandas as pd
 import myokit
 import myokit.lib.markov as markov
 
-# modelFile = 'clancy-1999.mmt'
-modelFile = "sanpy/models/tentusscher-2006.mmt"
-model, protocol, script = myokit.load(modelFile)
+def run_my_myokit():
 
-print("protocol:", protocol)
-"""
-# Level  Start    Length   Period   Multiplier
-1.0      50.0     0.5      1000.0   0
-"""
+    # modelFile = 'clancy-1999.mmt'
+    modelFile = "sanpy/models/tentusscher-2006.mmt"
+    model, protocol, script = myokit.load(modelFile)
 
-# duration in ms
-mySimDur = 30000
+    print("protocol:", protocol)
+    """
+    # Level  Start    Length   Period   Multiplier
+    1.0      50.0     0.5      1000.0   0
+    """
 
-# create a new protocol
-if 1:
-    protocol = myokit.Protocol()
+    # duration in ms
+    mySimDur = 30000
 
-    level = 1.0
-    duration = 0.5
+    # create a new protocol
+    if 1:
+        protocol = myokit.Protocol()
 
-    level = 0.5
-    duration = 1.5
+        level = 1.0
+        duration = 0.5
 
-    # 0 is the mean of the normal distribution you are choosing from
-    # 1 is the standard deviation of the normal distribution
-    # 100 is the number of elements you get in array noise
-    startTimes = np.linspace(0, mySimDur, 20)
-    # startNoise = np.random.normal(0,1,20)
-    startTimes = [x + abs(np.random.normal(500, 500, 1)) for x in startTimes]
-    print("startTimes:", startTimes)
-    for start in startTimes:
-        protocol.schedule(level, start, duration, period=0, multiplier=0)
+        level = 0.5
+        duration = 1.5
+
+        # 0 is the mean of the normal distribution you are choosing from
+        # 1 is the standard deviation of the normal distribution
+        # 100 is the number of elements you get in array noise
+        startTimes = np.linspace(0, mySimDur, 20)
+        # startNoise = np.random.normal(0,1,20)
+        startTimes = [x + abs(np.random.normal(500, 500, 1)) for x in startTimes]
+        print("startTimes:", startTimes)
+        for start in startTimes:
+            protocol.schedule(level, start, duration, period=0, multiplier=0)
 
 
-sim = myokit.Simulation(model, protocol)
+    sim = myokit.Simulation(model, protocol)
 
-# by default simulation run return variable time step
-# this is not compatible with 'real- recordings
-log_interval = 0.1  # default is None, units are ms ???
-d = sim.run(mySimDur, log_interval=log_interval)
+    # by default simulation run return variable time step
+    # this is not compatible with 'real- recordings
+    log_interval = 0.1  # default is None, units are ms ???
+    d = sim.run(mySimDur, log_interval=log_interval)
 
-import matplotlib.pyplot as plt
+    import matplotlib.pyplot as plt
 
-plt.figure()
-plt.plot(d["engine.time"], d["membrane.V"])
-plt.show()
-
-saveFile = "tentusscher.csv"
-df = pd.DataFrame(columns=["s", "mV"])
-timeSec = [x / 1000 for x in d["engine.time"]]
-
-# TODO: Timesteps are not constant! This trips up my analysis
-if 0:
-    tmpDiff = np.diff(d["engine.time"])
-    plt.plot(tmpDiff)
+    plt.figure()
+    plt.plot(d["engine.time"], d["membrane.V"])
     plt.show()
 
-df["s"] = timeSec
-df["mV"] = d["membrane.V"]
-print("saving:", saveFile)
-print(df.head())
-print(df.tail())
-df.to_csv(saveFile, index=False)
+    saveFile = "tentusscher.csv"
+    df = pd.DataFrame(columns=["s", "mV"])
+    timeSec = [x / 1000 for x in d["engine.time"]]
+
+    # TODO: Timesteps are not constant! This trips up my analysis
+    if 0:
+        tmpDiff = np.diff(d["engine.time"])
+        plt.plot(tmpDiff)
+        plt.show()
+
+    df["s"] = timeSec
+    df["mV"] = d["membrane.V"]
+    print("saving:", saveFile)
+    print(df.head())
+    print(df.tail())
+    df.to_csv(saveFile, index=False)
+
+if __name__ == '__main__':
+    run_my_myokit()
